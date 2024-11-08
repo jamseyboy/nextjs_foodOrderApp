738ce174d71c4abdaa8aaae55371f83f255a52b4
jamseyboy <buongahrangchal@gmail.com>
2024-11-08 09:08:42 +0530

    searchparams error fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Suspense>
         {children}
+        </Suspense>
+        
       </body>
     </html>
   );
